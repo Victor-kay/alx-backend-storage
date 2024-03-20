39a5d3dc56cdded6db7e98e1934858a253e48b6a
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-20 15:36:11 +0000

    revised codes

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,8 @@
--- 3-glam_rock.sql
 -- Task: List all bands with Glam rock as their main style, ranked by their longevity
 
--- List all bands with Glam rock as their main style, ranked by their longevity
+-- List bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, 
-       IFNULL(DATEDIFF(2022, formed), 0) - IFNULL(DATEDIFF(2022, split), 0) AS lifespan
+       TIMESTAMPDIFF(YEAR, formed, IFNULL(split, '2022-01-01')) AS lifespan
 FROM bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
